15814ceaa929841c3fc0a86ff623747daab8c13d
Patrick Baker <bakerpj1992@gmail.com>
2020-02-16 15:20:08 -0600

    create headline and change header background

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 // components to import
 import HomePage from '../HomePage/HomePage';
@@ -14,22 +14,27 @@ class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <Route path="/" exact component={HomePage}></Route>
-        <Route path="/feeling" render={() => <Form page='1' formType='FEELING' prevPage='/' nextPage='/understanding' formHeader='How are you feeling today?' />}></Route>
-        <Route path="/understanding" render={() => <Form page='2' formType='UNDERSTANDING' prevPage='/feeling' nextPage='/supported' formHeader='How well are you understanding the content?' />}></Route>
-        <Route path="/supported" render={() => <Form page='3' formType='SUPPORTED' prevPage='/understanding' nextPage='/comments' formHeader='How well are you being supported?' />}></Route>
-        <Route path="/comments" render={() => <Form page='4' formType='COMMENTS' prevPage='/supported' nextPage='/review' formHeader='Any comments you want to leave?' />}></Route>
-        <Route path="/review" component={Review}></Route>
-        <Route path="/success" component={Success}></Route>
-        <Route path="/admin" component={Admin}></Route>
-        <br/>
-      </div>
-      <pre>{JSON.stringify(this.props, null, 2)}</pre>
+        <div className="App">
+          <div id="headline">
+            <h4>Welcome to your classroom portal! We would love your feedback!</h4>
+          </div>
+          <div id="header">
+            <div className="App-title">
+              <h1>Feedback!</h1>
+              <h4><i>Don't forget it!</i></h4>
+            </div>
+          </div>
+          <Route path="/" exact component={HomePage}></Route>
+          <Route path="/feeling" render={() => <Form page='1' formType='FEELING' prevPage='/' nextPage='/understanding' formHeader='How are you feeling today?' />}></Route>
+          <Route path="/understanding" render={() => <Form page='2' formType='UNDERSTANDING' prevPage='/feeling' nextPage='/supported' formHeader='How well are you understanding the content?' />}></Route>
+          <Route path="/supported" render={() => <Form page='3' formType='SUPPORTED' prevPage='/understanding' nextPage='/comments' formHeader='How well are you being supported?' />}></Route>
+          <Route path="/comments" render={() => <Form page='4' formType='COMMENTS' prevPage='/supported' nextPage='/review' formHeader='Any comments you want to leave?' />}></Route>
+          <Route path="/review" component={Review}></Route>
+          <Route path="/success" component={Success}></Route>
+          <Route path="/admin" component={Admin}></Route>
+          <br />
+        </div>
+        {/* <pre>{JSON.stringify(this.props, null, 2)}</pre> */}
       </Router>
     );
   }
